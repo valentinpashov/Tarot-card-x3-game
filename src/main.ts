d8d6a86a210c7601fd371ecdfc8baffd25fc904c
Valentin Pashov <valentinpashov04@gmail.com>
2025-12-02 02:33:52 +0200

    Refactor main.ts to simplify PIXI.Application initialization and improve responsiveness handling

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,19 @@ import './style.css'
 import * as PIXI from 'pixi.js';
 import { Game } from './Game';
 
-(async () => {
-    const app = new PIXI.Application();
-    await app.init({ 
-        width: window.innerWidth, 
-        height: window.innerHeight,
-        backgroundColor: 0x000000,
-        resizeTo: window, 
-        resolution: window.devicePixelRatio || 1,
-        autoDensity: true
-    });
+const app = new PIXI.Application({ 
+    width: window.innerWidth, 
+    height: window.innerHeight,
+    backgroundColor: 0x000000,
+    resizeTo: window, 
+    resolution: window.devicePixelRatio || 1,
+    autoDensity: true,
+});
 
-    document.body.appendChild(app.canvas);
+document.body.appendChild(app.view as HTMLCanvasElement);
 
-    const game = new Game(app);
+const game = new Game(app);
 
-    //Responsive 
-    app.renderer.on('resize', () => {
-        game.resize(app.screen.width, app.screen.height);
-    });
-})();
+app.renderer.on('resize', () => {
+    game.resize(app.screen.width, app.screen.height);
+});
